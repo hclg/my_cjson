ef67e1113302749276d14abeb1d9af5d17f63868
hclg <2249121951@qq.com>
2020-05-11 23:57:46 +0800

    Utf16转32字符编码

--- cjson.c
+++ cjson.c
@@ -235,7 +235,79 @@ static const char *parse_string(cjson *item, const char *str) {
       ++ptr;
   out = (char *)cjson_malloc(len + 1);
   if (!out) return 0;
+  
+  ptr = str+1;
+  ptr2 = out;
+  while (*ptr != '\"' && *ptr) {
+    if (*ptr != '\\')
+      *ptr2++ = *ptr++;
+    else {
+      ++ptr;
+      switch (*ptr) {
+      case 'b':
+        *ptr2++ = '\b';
+        break;
+      case 'f':
+        *ptr2++ = '\f';
+        break;
+      case 'n':
+        *ptr2++ = '\n';
+        break;
+      case 'r':
+        *ptr2++ = '\r';
+        break;
+      case 't':
+        *ptr2++ = '\t';
+        break;
+      case 'u':
+        uc = parse_hex4(ptr+1);
+        ptr += 4;
 
+        if ((uc >= 0xDC00 && uc <= 0xDFFF) || uc == 0) break;
+        if (uc >= 0xD800 && uc <= 0xDBFF)/*UTF16 代理对*/ {
+          if (ptr[1] != '\\' || ptr[2] != 'u') break;/*缺少下半部分*/
+          uc2 = parse_hex4(ptr+3);
+          ptr += 6;
+          if (uc2 < 0xDC00 || uc2 > 0xDFFF) break;
+          uc = 0x10000 + (((uc & 0x3FF) << 10) | (uc2 & 0x3FF));
+
+        }
+
+        len = 4;
+        if (uc < 0x80) len = 1;
+        else if (uc < 0x800)  len = 2;
+        else if (uc < 0x10000) len = 3;
+        ptr2 += len;
+
+        switch (len)
+        {
+        case 4:
+          *--ptr2 = ((uc | 0x80) & 0xBF);
+          uc >>= 6;
+        case 3:
+          *--ptr2 = ((uc | 0x80) & 0xBF);
+          uc >>= 6;
+        case 2:
+          *--ptr2 = ((uc | 0x80) & 0xBF);
+          uc >>= 6;
+        case 1:
+          *--ptr2 = (uc | firstByteMark[len]);
+        }
+        ptr2 += len;
+        break;
+        default :
+          *ptr2++ = *ptr;
+          break;
+      }
+      ++ptr;
+    }
+  }
+  *ptr2 = 0;
+  if (*ptr == '\"')
+    ++ptr;
+  item->valuestring = out;
+  item->type = cjson_String;
+  return ptr;
 }
 
 // int main() {
